dd0206a986d76f64651a7e19875a8daa2ba33486
brian <b384li@uwaterloo.ca>
2021-11-10 11:54:57 -0500

    comments

--- src/explanations.py
+++ src/explanations.py
@@ -1,8 +1,3 @@
-'''
-Script for any explainability functions.
-Example: ab-classifier used GradCAM
-'''
-
 import yaml
 import os
 import cv2
@@ -10,11 +5,10 @@ import shutil
 import numpy as np
 import pandas as pd
 import tensorflow as tf
-from tensorflow.keras.models import load_model
-
 from scipy import ndimage
+from tensorflow.keras.models import load_model
+from tensorflow_addons.metrics import FBetaScore
 from models.models import *
-from tensorflow_addons.metrics import F1Score, FBetaScore
 from custom.metrics import Specificity
 from preprocessor import Preprocessor
 
@@ -22,6 +16,14 @@ cfg = yaml.full_load(open(os.path.join(os.getcwd(),"../config.yml"), 'r'))
 
 
 class ClipGradCam3D:
+
+    '''
+    Applies Grad-CAM and saves resulting heatmap frames as videos for 3D CNNs ONLY
+    Necessary components:
+        3D CNN in Tensorflow SavedModel format, in directory specified in config file
+        NPZ clips (30 FPS) in directory specified in config file
+        CSV containing 'id', 'label' (0 or 1) and 'filename' (full path) of NPZ clips to extract heatmaps from
+    '''
 
     def __init__(self):
         custom_objects = {'Specificity': Specificity, 'FBetaScore': FBetaScore}
@@ -34,7 +36,7 @@ class ClipGradCam3D:
         _, self.preprocessing_fn = get_model(cfg['TRAIN']['MODEL_DEF'])
         self.hm_intensity = 0.5
 
-        # make sure heatmap directory exists and is empty
+        # Make sure heatmap directory exists and is empty
         if os.path.exists(self.heatmap_dir):
             shutil.rmtree(self.heatmap_dir)
         os.makedirs(self.heatmap_dir)
@@ -47,6 +49,10 @@ class ClipGradCam3D:
         self.last_conv_layer = layer_name
 
     def apply_gradcam(self):
+
+        '''
+        Extracts and saves heatmaps for all listed mini-clips using NPZ CSV, with path specified in config file
+        '''
 
         # get dataset and apply preprocessor
         miniclip_csv = pd.read_csv(cfg['EXPLAINABILITY']['PATHS']['NPZ_DF'])
@@ -66,6 +72,7 @@ class ClipGradCam3D:
             # all mini-clips currently downsampled to 30 FPS
             frame_rate = 30
 
+            # using given input clip, pool gradients at final conv layer, giving a single float per filter
             with tf.GradientTape() as tape:
                 last_conv_layer = self.model.get_layer(self.last_conv_layer)
                 tape.watch(last_conv_layer.variables)
@@ -75,6 +82,7 @@ class ClipGradCam3D:
                 grads = tape.gradient(class_out, last_conv_layer)
                 pooled_grads = tf.keras.backend.mean(grads, axis=(0, 1, 2, 3))
 
+            # Incorporate filter relevance, remove filter dimension, and resize heatmap
             heatmap = tf.reduce_mean(tf.multiply(pooled_grads, last_conv_layer), axis=-1)
             heatmap = np.array(heatmap)
             heatmap = heatmap.squeeze(axis=0)
@@ -85,16 +93,27 @@ class ClipGradCam3D:
             heatmap /= np.max(heatmap)
             heatmap = np.uint8(255 * heatmap)
 
+            # Add colour to heatmap images (per-frame), and superimpose heatmaps onto original unaltered frames
             heatmap_imgs = []
             for hmap, frame in zip(heatmap, orig_clip):
                 hmap = cv2.applyColorMap(hmap, cv2.COLORMAP_HOT)
                 heatmap_imgs.append(cv2.addWeighted(hmap, self.hm_intensity, frame, 1.0 - self.hm_intensity, 0, dtype=cv2.CV_8U))
 
+            # Save heatmap frames as a single video
             self.save_heatmap_video(heatmap_imgs, id, frame_rate)
 
             index += 1
 
     def save_heatmap_video(self, heatmaps, id, fps):
+
+        '''
+        Saves a list of frame-wise heatmaps as a video (mp4)
+
+        :param heatmaps: List of heatmaps, one for each frame
+        :param id: ID of the corresponding LUS mini-clip
+        :param fps: Frame rate of the outputted video
+        '''
+
         size = (heatmaps[0].shape[0], heatmaps[0].shape[1])
         out = cv2.VideoWriter(self.heatmap_dir + id + '_heatmap.mp4', cv2.VideoWriter_fourcc(*'mp4v'), fps, size)
         for heatmap in heatmaps:
