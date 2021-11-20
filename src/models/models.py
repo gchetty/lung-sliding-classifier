de82bf7c8d3a743bcc9a4830e8a0557d1006ee29
brian <b384li@uwaterloo.ca>
2021-11-19 19:23:31 -0500

    flow is 0 to 255, should be normalized

--- src/models/models.py
+++ src/models/models.py
@@ -55,7 +55,7 @@ def get_model(model_name):
         preprocessing_fn = resnet_preprocess
 
     if flow:
-        preprocessing_fn = lambda x: x
+        preprocessing_fn = normalize
 
     return model_def_fn, preprocessing_fn
 
