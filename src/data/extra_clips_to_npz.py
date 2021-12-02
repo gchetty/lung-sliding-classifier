4f090fb1850aa0980fb7eed1cc1f4b1b21d1f3c1
brian <b384li@uwaterloo.ca>
2021-12-02 16:26:48 -0500

    refactor

--- src/data/extra_clips_to_npz.py
+++ src/data/extra_clips_to_npz.py
@@ -4,7 +4,218 @@ import pandas as pd
 import os
 import yaml
 from utils import refresh_folder
-from .to_npz import video_to_npz, flow_frames_to_npz_contig, flow_frames_to_npz_downsampled
+
+
+def video_to_frames_downsampled(orig_id, patient_id, df_rows, cap, fr, seq_length=cfg['PARAMS']['WINDOW'],
+                                resize=cfg['PARAMS']['IMG_SIZE'], write_path=''):
+    '''
+    Converts a LUS video file to mini-clips downsampled to 30 FPS with specified sequence length
+
+    :param orig_id: ID of the video file to be converted
+    :param patient_id: Patient ID corresponding to the video file
+    :param df_rows: list of (mini-clip_ID, patient_ID), updated in this function, and later downloaded
+    :param cap: Captured video of full clip, returned by cv2.VideoCapture()
+    :param fr: Frame rate (integer) of original clip - MUST be divisible by 30
+    :param seq_length: Length of each mini-clip
+    :param resize: [width, height], dimensions to resize frames to before saving
+    :param write_path: Path to directory where output mini-clips are saved
+    '''
+
+    # Check validity of frame rate param
+    assert (isinstance(fr, int))
+    assert (fr % 30 == 0)
+
+    frames = []
+    stride = fr // 30
+
+    index = 0  # Position in 'frames' array where the next frame is to be appended
+
+    try:
+        while True:
+            ret, frame = cap.read()
+            if not ret:
+                break
+            if index == 0:  # Add every nth frame only
+                frame = cv2.resize(frame, tuple(resize))
+                frames.append(frame)
+            index = (index + 1) % stride
+
+    finally:
+        cap.release()
+
+    # Assemble and save mini-clips from extracted frames
+    counter = 1
+    num_mini_clips = len(frames) // seq_length
+    for i in range(num_mini_clips):
+        df_rows.append([orig_id + '_' + str(counter), patient_id])
+        np.savez(write_path + '_' + str(counter), frames=frames[i * seq_length:i * seq_length + seq_length])
+        counter += 1
+
+    return
+
+
+def video_to_frames_contig(orig_id, patient_id, df_rows, cap, seq_length=cfg['PARAMS']['WINDOW'],
+                           resize=cfg['PARAMS']['IMG_SIZE'], write_path=''):
+    '''
+    Converts a LUS video file to contiguous-frame mini-clips with specified sequence length
+
+    :param orig_id: ID of the video file to be converted
+    :param patient_id: Patient ID corresponding to the video file
+    :param df_rows: list of (mini-clip_ID, patient_ID), updated in this function, and later downloaded
+    :param cap: Captured video of full clip, returned by cv2.VideoCapture()
+    :param seq_length: Length of each mini-clip
+    :param resize: [width, height], dimensions to resize frames to before saving
+    :param write_path: Path to directory where output mini-clips are saved
+    '''
+
+    counter = seq_length
+    mini_clip_num = 1  # nth mini-clip being made from the main clip
+    frames = []
+    try:
+        while True:
+
+            # When seq_length frames have been read, update df rows and write npz files
+            # The id of the xth mini-clip from a main clip is the id of the main clip with _x appended to it
+            if counter == 0:
+                df_rows.append(
+                    [orig_id + '_' + str(mini_clip_num), patient_id])  # append to what will make output dataframes
+                np.savez(write_path + '_' + str(mini_clip_num), frames=frames)  # output
+                counter = seq_length
+                mini_clip_num += 1
+                frames = []
+
+            ret, frame = cap.read()
+            if not ret:
+                break
+
+            frame = cv2.resize(frame, tuple(resize))
+            frames.append(frame)
+
+            counter -= 1
+
+    finally:
+        cap.release()
+
+    return
+
+
+def flow_frames_to_npz_downsampled(path, orig_id, patient_id, df_rows, fr, seq_length=cfg['PARAMS']['WINDOW'],
+                                   resize=cfg['PARAMS']['IMG_SIZE'], write_path=''):
+    '''
+    Converts a directory of x and y flow frames to mini-clips downsampled to 30 FPS, with flows stacked on axis = -1
+
+    :param path: Path to directory containing flow frames
+    :param orig_id: ID of the LUS video file associated with the flow frames
+    :param patient_id: Patient ID corresponding to the video file
+    :param df_rows: list of (mini-clip_ID, patient_ID), updated in this function, and later downloaded
+    :param fr: Frame rate (in FPS) of original LUS clip
+    :param seq_length: Length of each mini-clip
+    :param resize: [width, height], dimensions to resize frames to before saving
+    :param write_path: Path to directory where output mini-clips are saved
+    '''
+
+    frames_x = []
+    frames_y = []
+
+    stride = fr // 30
+
+    # Read all flow frames
+    for file in os.listdir(path):
+        frame = cv2.imread(os.path.join(path, file), 0)
+        frame = cv2.resize(frame, tuple(resize))
+        ind = int(file[7:12])  # flow frame number
+        if (ind - 1) % stride == 0:  # take every nth flow frame only
+            if '_x_' in file:
+                frames_x.append(frame)
+            else:
+                frames_y.append(frame)
+
+    counter = 1
+    num_mini_clips = len(frames_x) // seq_length
+
+    # Stack x and y flow (making 2 channels) and save mini-clip sequences
+    for i in range(num_mini_clips):
+        df_rows.append([orig_id + '_' + str(counter), patient_id])
+        x_seq = np.array(frames_x[i * seq_length:i * seq_length + seq_length])
+        y_seq = np.array(frames_y[i * seq_length:i * seq_length + seq_length])
+        np.savez(write_path + '_' + str(counter), frames=np.stack((x_seq, y_seq), axis=-1))
+        counter += 1
+
+    return
+
+
+def flow_frames_to_npz_contig(path, orig_id, patient_id, df_rows, seq_length=cfg['PARAMS']['WINDOW'],
+                              resize=cfg['PARAMS']['IMG_SIZE'], write_path=''):
+    '''
+    Converts a directory of x and y flow frames to contiguous mini-clips, with flows stacked on axis = -1
+
+    :param path: Path to directory containing flow frames
+    :param orig_id: ID of the LUS video file associated with the flow frames
+    :param patient_id: Patient ID corresponding to the video file
+    :param df_rows: list of (mini-clip_ID, patient_ID), updated in this function, and later downloaded
+    :param seq_length: Length of each mini-clip
+    :param resize: [width, height], dimensions to resize frames to before saving
+    :param write_path: Path to directory where output mini-clips are saved
+    '''
+
+    frames_x = []
+    frames_y = []
+
+    # Read all flow frames
+    for file in os.listdir(path):
+        frame = cv2.imread(os.path.join(path, file), 0)
+        frame = cv2.resize(frame, tuple(resize))
+        if '_x_' in file:
+            frames_x.append(frame)
+        else:
+            frames_y.append(frame)
+
+    counter = 1
+    num_mini_clips = len(frames_x) // seq_length
+
+    # Stack x and y flow (making 2 channels) and save mini-clip sequences
+    for i in range(num_mini_clips):
+        df_rows.append([orig_id + '_' + str(counter), patient_id])
+        x_seq = np.array(frames_x[i * seq_length:i * seq_length + seq_length])
+        y_seq = np.array(frames_y[i * seq_length:i * seq_length + seq_length])
+        np.savez(write_path + '_' + str(counter), frames=np.stack((x_seq, y_seq), axis=-1))
+        counter += 1
+
+    return
+
+
+def video_to_npz(path, orig_id, patient_id, df_rows, write_path='', method=cfg['PARAMS']['METHOD'], fr=None):
+    '''
+    Converts a LUS video file to mini-clips
+
+    :param path: Path to video file to be converted
+    :param orig_id: ID of the video file to be converted
+    :param patient_id: Patient ID corresponding to the video file
+    :param df_rows: list of (mini-clip_ID, patient_ID), updated in this function, and later downloaded
+    :param write_path: Path to directory where output mini-clips are saved
+    :param method: Method of frame extraction for mini-clips, either 'Contiguous' or ' Stride'
+    '''
+
+    flow = cfg['PARAMS']['FLOW']
+
+    cap = cv2.VideoCapture(path)
+
+    if not fr:
+        fr = round(cap.get(cv2.CAP_PROP_FPS)) \
+            # Disregard clips with undesired frame rate, only if frame rate not passed in (passed in = override checking)
+        if not (fr % 30 == 0):
+            return
+    else:  # cast frame rate to closest multiple of 30
+        fr = round(fr / 30.0) * 30.0
+
+    if method == 'Contiguous':
+        if fr == 30:
+            video_to_frames_contig(orig_id, patient_id, df_rows, cap, write_path=write_path)
+        else:
+            video_to_frames_downsampled(orig_id, patient_id, df_rows, cap, fr, write_path=write_path)
+    else:
+        raise Exception('Stride method not yet implemented!')
+
 
 cfg = yaml.full_load(open(os.path.join(os.getcwd(),"../../config.yml"), 'r'))['PREPROCESS']
 
