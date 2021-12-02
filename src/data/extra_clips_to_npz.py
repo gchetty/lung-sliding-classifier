e8a6fdfb2ff599472d52564508d92ef6c99a60df
brian <b384li@uwaterloo.ca>
2021-12-02 11:17:55 -0500

    flow script

--- src/data/extra_clips_to_npz.py
+++ src/data/extra_clips_to_npz.py
@@ -8,3 +8,15 @@ from utils import refresh_folder
 cfg = yaml.full_load(open(os.path.join(os.getcwd(),"../../config.yml"), 'r'))['PREPROCESS']
 
 
+flow = cfg['PARAMS']['FLOW']
+
+if not (flow == 'Yes'):
+    input_folder = cfg['PATHS']['EXTRA_CROPPED_VIDEOS']
+    npz_folder = cfg['PATHS']['EXTRA_NPZ']
+    refresh_folder(npz_folder)
+
+if not (flow == 'No'):
+    flow_input_folder = cfg['PATHS']['EXTRA_FLOW_VIDEOS']
+    flow_npz_folder = cfg['PATHS']['EXTRA_FLOW_NPZ']
+    refresh_folder(flow_npz_folder)
+
