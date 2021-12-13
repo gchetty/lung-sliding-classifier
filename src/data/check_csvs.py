1570868382b7dd3341cad7f601959d9d9f931f55
brian <b384li@uwaterloo.ca>
2021-12-13 00:45:13 -0500

    .

--- src/data/check_csvs.py
+++ src/data/check_csvs.py
@@ -9,6 +9,7 @@ for index, row in df.iterrows():
     if not (os.path.exists(os.path.join('flow_npzs/no_sliding/', row['id'] + '.npz')) or os.path.exists(os.path.join('flow_npzs/orig_no_sliding/', row['id'] + '.npz'))):
         indices.append(index)
 
+print('Dropping ' + str(len(indices)) + ' bad entries')
 df = df.drop(indices)
 
 df.to_csv('csvs/no_sliding_flow_mini_clips.csv', index=False)
