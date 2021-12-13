0b6656d60083c2d943254d4c3ac63f671b48d736
brian <b384li@uwaterloo.ca>
2021-12-13 13:50:29 -0500

    fix flow csv

--- src/data/check_csvs.py
+++ src/data/check_csvs.py
@@ -1,15 +1,15 @@
 import os
 import pandas as pd
 
-df = pd.read_csv('csvs/no_sliding_flow_mini_clips.csv')
+df = pd.read_csv('csvs/sliding_flow_mini_clips.csv')
 
 indices = []
 
 for index, row in df.iterrows():
-    if not (os.path.exists(os.path.join('flow_npzs/no_sliding/', row['id'] + '.npz')) or os.path.exists(os.path.join('flow_npzs/orig_no_sliding/', row['id'] + '.npz'))):
+    if not os.path.exists(os.path.join('flow_npzs/sliding/', row['id'] + '.npz')):
         indices.append(index)
 
 print('Dropping ' + str(len(indices)) + ' bad entries')
 df = df.drop(indices)
 
-df.to_csv('csvs/no_sliding_flow_mini_clips.csv', index=False)
+df.to_csv('csvs/sliding_flow_mini_clips.csv', index=False)
